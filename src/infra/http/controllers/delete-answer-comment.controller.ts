6f7c29ccb4692a31a855915219d9b2713a53d01b
Alfiado Constantino <alfiado.ngana@xit2s.com>
2026-01-13 20:50:42 +0200

    Controller: deletar comentario da resposta

---
+++ src/infra/http/controllers/delete-answer-comment.controller.ts
@@ -0,0 +1,32 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+
+import type { TokenPayloadSchema } from '@/infra/auth/jwt.strategy'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+
+@Controller('answers/comments/:id')
+export class DeleteAnswerCommentController {
+  constructor(private deleteAnswerCommentUseCase: DeleteAnswerCommentUseCase) {} // eslint-disable-line
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: TokenPayloadSchema,
+    @Param('id') answerCommentId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.deleteAnswerCommentUseCase.execute({
+      answerCommentId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) throw new BadRequestException()
+  }
+}
