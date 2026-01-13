b8768449bff9614c0758f4868aea6f97f6397620
Alfiado Constantino <alfiado.ngana@xit2s.com>
2026-01-13 21:31:53 +0200

    Controller: listar comentarios da pergunta

---
+++ src/infra/http/controllers/fetch-question-comment.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
+import { z } from 'zod'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const pageQueryParamsSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamsSchema = z.infer<typeof pageQueryParamsSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema)
+
+@Controller('questions/:questionId/comments')
+export class FetchQuestionCommentController {
+  constructor(private fetchQuestionCommentUseCase: FetchQuestionCommentsUseCase) {} // eslint-disable-line
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamsSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionCommentUseCase.execute({
+      page,
+      questionId,
+    })
+
+    if (result.isLeft()) throw new BadRequestException()
+
+    return {
+      comments: result.value.questionComments.map(CommentPresenter.toHTTP),
+    }
+  }
+}
