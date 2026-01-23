f0f7b648c6e9de5e81de8634c4747e4191945f1f
Alfiado Constantino <alfiado.ngana@xit2s.com>
2026-01-23 02:39:32 +0200

    criando service do redis

--- src/infra/env/env.service.ts
+++ src/infra/env/env.service.ts
@@ -4,9 +4,9 @@ import type { Env } from './env'
 
 @Injectable()
 export class EnvService {
-  constructor(private configService: ConfigService<Env, true>) { } // eslint-disable-line
+  constructor(private configService: ConfigService<Env, true>) {} // eslint-disable-line
 
   get<T extends keyof Env>(key: T) {
-    return this.configService.get<T>(key, { infer: true })
+    return this.configService.get(key, { infer: true })
   }
 }
